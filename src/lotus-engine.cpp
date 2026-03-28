9d63318e9643972699454497f1677cbbf327145b
hthienloc <huynhloc.contact@gmail.com>
2026-03-29 01:33:37 +0700

    clang

--- src/lotus-engine.cpp
+++ src/lotus-engine.cpp
@@ -186,11 +186,7 @@ namespace fcitx {
 #ifndef DISABLE_VERSION_ACTION
             versionAction_.get(),
 #endif
-            charsetAction_.get(),
-            spellCheckAction_.get(),
-            macroAction_.get(),
-            capitalizeMacroAction_.get(),
-            autoNonVnRestoreAction_.get()};
+            charsetAction_.get(), spellCheckAction_.get(), macroAction_.get(), capitalizeMacroAction_.get(), autoNonVnRestoreAction_.get()};
     }
 
     void LotusEngine::initToggleAction(std::unique_ptr<SimpleAction>& action, Option<bool>& option, const std::string& actionId, const std::string& iconName,
